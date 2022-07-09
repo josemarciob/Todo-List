62fee709c4b36cf5caef71f81744a7e9951811ab
Marciovesk <jjmarcio159@gmail.com>
2022-07-09 14:40:44 -0300

    Added Filter Tool: All, Completed, Uncompleted

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteCheck)
+filterOption.addEventListener('click', filterTodo)
 
 //Functions
 function addTodo(event) {
@@ -43,7 +45,6 @@ function addTodo(event) {
 
 function deleteCheck(event) {
   const item = event.target
-
   //DELETE TODO
   if (item.classList[0] === 'trash-btn') {
     const todo = item.parentElement
@@ -53,10 +54,34 @@ function deleteCheck(event) {
       todo.remove()
     })
   }
-
   //CHECK MARK
   if (item.classList[0] === 'complete-btn') {
     const todo = item.parentElement
     todo.classList.toggle('completed')
   }
+}
+
+function filterTodo(event) {
+  const todos = todoList.childNodes
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case 'all':
+        todo.style.display = 'flex'
+        break
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex'
+        } else {
+          todo.style.display = 'none'
+        }
+        break
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex'
+        } else {
+          todo.style.display = 'none'
+        }
+        break
+    }
+  })
 }
